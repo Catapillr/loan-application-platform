da37d40f1f2f904b7f57de43d7092f46ab86d741
Max Gerber <mgerber94@gmail.com>
2019-08-27 18:03:53 +0100

    Styling of Welcome screen
    Relates #17

--- pages/onboarding/Step1.js
+++ pages/onboarding/Step1.js
@@ -1,6 +1,20 @@
 import { Formik, Form, Field, ErrorMessage } from "formik"
 import * as Yup from "yup"
+import { Heading, Copy, Button } from "./styles"
 
-const Step1 = () => <div></div>
+const Step1 = () => (
+  <main className="flex justify-center items-center flex-col">
+    {/* <main> */}
+    <Heading className="mb-6 text-center">Welcome to Catapillr!</Heading>
+    <Copy className="mb-12">
+      We've partnered up with{" "}
+      <span className="font-bold">London Metropolitan University</span> to offer
+      you interest-free childcare loans to help tide you over and hopefully make
+      both your work life and your home life more rewarding. Press the button
+      below to see how much you could borrow from your employer!
+    </Copy>
+    <Button className="text-center">Let's get started</Button>
+  </main>
+)
 
 export default Step1
