d6c132d0875a830379d2e361b94cf0806d616f1c
Ivan <ivangonzalez@rocketmail.com>
2019-08-30 18:26:38 +0100

    Update prisma seeds

--- prisma/seeds.js
+++ prisma/seeds.js
@@ -3,20 +3,48 @@
 
 const { prisma } = require("./generated")
 
-prisma
-  .createEmployer({
-    name: "yalla",
-    slug: "yalla",
-    emailSuffix: "@yallacooperative.com",
-  })
-  .then(res => console.log(JSON.stringify(res, undefined, 2))) //eslint-disable-line no-console
-  .catch(res => console.log(JSON.stringify(res, undefined, 2))) //eslint-disable-line no-console
+const seedDatabase = async () => {
+  try {
+    const deleteUsers = await prisma.deleteManyUsers({
+      id_not: 0,
+    })
 
-prisma
-  .createEmployer({
-    name: "infact",
-    slug: "infact",
-    emailSuffix: "@infactcoop.com",
-  })
-  .then(res => console.log(JSON.stringify(res, undefined, 2))) //eslint-disable-line no-console
-  .catch(res => console.log(JSON.stringify(res, undefined, 2))) //eslint-disable-line no-console
+    const deleteEmployers = await prisma.deleteManyEmployers({
+      id_not: 0,
+    })
+
+    const yalla = await prisma.createEmployer({
+      name: "yalla",
+      slug: "yalla",
+      emailSuffix: "@yallacooperative.com",
+    })
+
+    const infact = await prisma.createEmployer({
+      name: "infact",
+      slug: "infact",
+      emailSuffix: "@infactcoop.com",
+    })
+
+    const ivan = await prisma.createUser({
+      firstName: "Ivan",
+      lastName: "Gonzalez",
+      email: "ivan@infactcoop.com",
+      phoneNumber: new Date().toISOString(),
+      dob: new Date().toISOString(),
+      nationality: "Colombian",
+      employmentStartDate: new Date().toISOString(),
+      annualSalary: 28392382,
+      employer: { connect: { id: infact.id } },
+    })
+
+    console.log(JSON.stringify(deleteEmployers, undefined, 2)) //eslint-disable-line no-console
+    console.log(JSON.stringify(deleteUsers, undefined, 2)) //eslint-disable-line no-console
+    console.log(JSON.stringify(yalla, undefined, 2)) //eslint-disable-line no-console
+    console.log(JSON.stringify(infact, undefined, 2)) //eslint-disable-line no-console
+    console.log(JSON.stringify(ivan, undefined, 2)) //eslint-disable-line no-console
+  } catch (e) {
+    console.log(JSON.stringify(e, undefined, 2)) //eslint-disable-line no-console
+  }
+}
+
+seedDatabase()
