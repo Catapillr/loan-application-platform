f26db8ee8ca2aceccb84f8fcbe9467b7b82a2a9d
Max Gerber <mgerber94@gmail.com>
2019-09-16 13:14:37 +0100

    + Add telephone parsing library  + Complete sync telephone validation checks
    Relates #80

--- components/onboarding/Step7Personal.js
+++ components/onboarding/Step7Personal.js
@@ -1,4 +1,5 @@
 import * as Yup from "yup"
+import { parsePhoneNumberFromString } from "libphonenumber-js"
 
 import Questions from "./Questions"
 import { TextInput, SelectInput } from "../../components/Input"
@@ -9,11 +10,23 @@ import nationalityOptions from "./nationalityOptions"
 
 const validation = Yup.object().shape({
   nationality: Yup.string().required("Required"),
-  phoneNumber: Yup.string()
-    .required("Required")
-    .min(10, "Please enter a valid phone number")
-    .max(15, "Please enter a valid phone number"),
+  phoneNumber: Yup.string().required("Required"),
 })
+
+const validatePhoneNumber = async value => {
+  const phoneNumber = parsePhoneNumberFromString(value, "GB")
+  console.log("phoneNumber", phoneNumber)
+  if (!phoneNumber) {
+    return "Please enter a complete phone number."
+  }
+  if (phoneNumber.countryCallingCode !== "44") {
+    return "Please enter a valid UK number"
+  }
+  if (!phoneNumber.isValid()) {
+    return "Sorry, this phone number is not valid."
+  }
+
+}
 
 const Step7 = () => (
   <Questions
@@ -39,6 +52,7 @@ const Step7 = () => (
         component: TextInput,
         name: "phoneNumber",
         width: "full",
+        validate: value => validatePhoneNumber(value),
       },
     ]}
   />
