caab765a8b36b48ac89de42084b080dd6fda80d5
lucymk <lucy.m.king92@gmail.com>
2019-10-23 12:28:00 +0100

    removes hardcoded Jessica

--- components/onboarding/register-as-a-provider/Welcome.js
+++ components/onboarding/register-as-a-provider/Welcome.js
@@ -23,7 +23,8 @@ const Welcome = ({ user, paymentRequest, incrementPage }) => (
       `}
     </Heading>
     <Copy className="mb-6">
-      We just need a few details from you to allow Jessica to pay you using her{" "}
+      We just need a few details from you to allow ${user.firstName} to pay you
+      using her{" "}
       <Link href="/" target="_blank">
         Catapillr interest-free childcare loan
       </Link>
