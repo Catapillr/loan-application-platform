817383a0236e4fa69dcb25df1bb03ddab4dfe2f6
Max Gerber <mgerber94@gmail.com>
2020-01-22 17:09:01 +0000

    + Add copy change re current employee

--- components/multistep-forms/employee/Eligibility.js
+++ components/multistep-forms/employee/Eligibility.js
@@ -89,15 +89,14 @@ const Eligibility = ({
   values: { employmentStartDate },
   setFieldValue,
 }) => {
-  const { emailSuffixes, minimumServiceLength } = employer
+  const { emailSuffixes, minimumServiceLength, name } = employer
   return (
     <Questions
       formWidth="70"
       title="To confirm your eligibility for the scheme, please answer the following questions."
       questions={[
         {
-          text:
-            'The Childcare Cash Advance Scheme is currently only available for permanent employees. Please click the circle below to confirm you are a permanent employee.',
+          text: `The Childcare Cash Advance Scheme is currently only available for permanent employees. Please click the circle below to confirm you are a permanent employee of ${name}.`,
           name: 'permanentRole',
           className: '',
           type: 'checkbox',
