4c78c33585cb7103e6ebd719fb175421311de899
Max Gerber <mgerber94@gmail.com>
2020-01-22 17:09:36 +0000

    "current employee"

--- components/multistep-forms/employee/Eligibility.js
+++ components/multistep-forms/employee/Eligibility.js
@@ -18,7 +18,7 @@ const validation = Yup.object().shape({
   employmentStartDate: Yup.object().required(),
   permanentRole: Yup.boolean().oneOf(
     [true],
-    'Sorry, you must be in a permanent role to apply!',
+    'Sorry, you must be a current employee to apply!',
   ),
 })
 
@@ -96,7 +96,7 @@ const Eligibility = ({
       title="To confirm your eligibility for the scheme, please answer the following questions."
       questions={[
         {
-          text: `The Childcare Cash Advance Scheme is currently only available for permanent employees. Please click the circle below to confirm you are a permanent employee of ${name}.`,
+          text: `The Childcare Cash Advance Scheme is currently only available for current employees. Please click the circle below to confirm you are a current employee of ${name}.`,
           name: 'permanentRole',
           className: '',
           type: 'checkbox',
